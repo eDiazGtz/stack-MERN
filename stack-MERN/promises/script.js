176338a5a3ed21b1017d1dd039b4ce6f603af2a7
Edgar Diaz-Gutierrez <edgar.s.dzgz@gmail.com>
2021-11-14 11:10:01 -0500

    finished SWAPI assignment

--- stack-MERN/promises/script.js
+++ stack-MERN/promises/script.js
@@ -4,9 +4,25 @@
 console.log("May the force be with you!");
 
 fetch("https://swapi.dev/api/planets/1/") // success | failure | pending       are the three states of promises
+
 // successful response
+    .then((response) => {
+        console.log(response)
+        // When success we can convert response to JSON to use
+        response.json()
+            .then((jsonResponse) => {
+                console.log(jsonResponse)
+            })
+            .catch((jsonError) => {
+                console.log((jsonError))
+            })
+    })
 // failure / error response
+    .catch((error) => {
+        console.log(error)
+    })
 // promises are async
+// Below is NOT ASYNC
 
 console.log(Date.now().toLocaleString());
 console.log("Meditating on life while waiting for SWAPI to return data!");
